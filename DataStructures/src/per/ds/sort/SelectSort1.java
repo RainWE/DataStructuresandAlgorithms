5509863049cabef3d3ccb433cfdb624154ae025c
CDX <1138224907@qq.com>
2020-06-20 15:31:44 +0800

    算法：选择排序

---
+++ DataStructures/src/per/ds/sort/SelectSort1.java
@@ -0,0 +1,139 @@
+package per.ds.sort;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+//选择排序
+public class SelectSort1 {
+
+    public static void main(String[] args) {
+        //int [] arr = {101, 34, 119, 1, -1, 90, 123};
+
+        //创建要给80000个的随机的数组
+        int[] arr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
+        }
+
+        System.out.println("排序前");
+        //System.out.println(Arrays.toString(arr));
+
+        Date data1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str = simpleDateFormat.format(data1);
+        System.out.println("排序前的时间是=" + date1Str);
+
+        selectSort(arr);
+
+
+        Date data2 = new Date();
+        String date2Str = simpleDateFormat.format(data2);
+        System.out.println("排序前的时间是=" + date2Str);
+
+        //System.out.println("排序后");
+        //System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    //选择排序
+    public static void selectSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            int min = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+				if(min>arr[j]){
+				    min=arr[j];
+				    minIndex=j;
+                }
+            }
+            if(minIndex!=i){
+                arr[minIndex]=arr[i];
+                arr[i]=min;
+            }
+            //System.out.println("第"+(i+1)+"轮后~~");
+            //System.out.println(Arrays.toString(arr));// 1, 34, 119, 101
+        }
+    }
+
+    public static void selectSort_1(int[] arr) {
+        for(int i=0;i<arr.length-1;i++){
+            int min=arr[i];
+            int minIndex= i;
+            for (int j= i+1;j<arr.length;j++){
+                if(min>arr[j]){
+                    min =arr[j];
+                    minIndex=j;
+                }
+            }
+            if(minIndex!=i){
+                arr[minIndex]=arr[i];
+                arr[i]=min;
+            }
+        }
+    }
+    public static void selectSort_2(int[] arr) {
+        for(int i=0;i<arr.length-1;i++){
+            int min = arr[i];
+            int minIndex= i;
+            for(int j=i+1;j<arr.length;j++){
+                if(min >arr[j]){
+                    min = arr[j];
+                    minIndex=j;
+                }
+            }
+            if(minIndex!=i){
+                arr[minIndex]=arr[i];
+                arr[i]=min;
+            }
+        }
+    }
+    public static void selectSort_3(int[] arr) {
+        for(int i=0;i<arr.length-1;i++){
+            int min=arr[i];
+            int minIndex =i;
+            for(int j=i+1;j<arr.length;j++){
+                if(min>arr[j]){
+                    min=arr[j];
+                    minIndex=j;
+                }
+            }
+            if(minIndex!=i){
+                arr[minIndex]=arr[i];
+                arr[i]=min;
+            }
+        }
+    }
+    public static void selectSort_4(int[] arr) {
+        for(int i=0;i<arr.length-1;i++){
+            int min =arr[i];
+            int minIndex= i;
+            for(int j =i+1;j<arr.length;j++){
+                if(min>arr[j]){
+                    min=arr[j];
+                    minIndex=j;
+                }
+            }
+            if(minIndex!=i){
+                arr[minIndex]=arr[i];
+                arr[i]=min;
+            }
+        }
+    }
+    public static void selectSort_5(int[] arr) {
+        for (int i=0;i<arr.length-1;i++){
+            int min = arr[i];
+            int minIndex = i;
+            for(int j=i+1;j<arr.length;j++){
+                if(min >arr[j]){
+                    min =arr[j];
+                    minIndex=j;
+                }
+            }
+            if(minIndex!=i){
+                arr[minIndex]=arr[i];
+                arr[i]=min;
+            }
+        }
+    }
+}
