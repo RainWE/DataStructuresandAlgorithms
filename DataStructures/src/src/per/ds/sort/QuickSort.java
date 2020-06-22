6b0491b199cf7904cd00493f264e704dbd38131e
CDX <1138224907@qq.com>
2020-06-22 17:04:56 +0800

    算法：快速排序

--- DataStructures/src/src/per/ds/sort/QuickSort.java
+++ DataStructures/src/src/per/ds/sort/QuickSort.java
@@ -7,27 +7,28 @@ import java.util.Date;
 public class QuickSort {
 
 	public static void main(String[] args) {
-		//int[] arr = {-9,78,0,23,-567,70, -1,900, 4561};
-		
+//		int[] arr = {-9,78,0,23,-567,70, -1,900, 4561};
+		int[] arr = {7,3,6,10,6};
+
 		//测试快排的执行速度
 		// 创建要给80000个的随机的数组
-		int[] arr = new int[8000000];
-		for (int i = 0; i < 8000000; i++) {
-			arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
-		}
-		
-		System.out.println("排序前");
-		Date data1 = new Date();
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		String date1Str = simpleDateFormat.format(data1);
-		System.out.println("排序前的时间是=" + date1Str);
+//		int[] arr = new int[8000000];
+//		for (int i = 0; i < 8000000; i++) {
+//			arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
+//		}
+//
+//		System.out.println("排序前");
+//		Date data1 = new Date();
+//		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//		String date1Str = simpleDateFormat.format(data1);
+//		System.out.println("排序前的时间是=" + date1Str);
 		
 		quickSort(arr, 0, arr.length-1);
 		
-		Date data2 = new Date();
-		String date2Str = simpleDateFormat.format(data2);
-		System.out.println("排序前的时间是=" + date2Str);
-		//System.out.println("arr=" + Arrays.toString(arr));
+//		Date data2 = new Date();
+//		String date2Str = simpleDateFormat.format(data2);
+//		System.out.println("排序前的时间是=" + date2Str);
+		System.out.println("arr=" + Arrays.toString(arr));
 	}
 
 	public static void quickSort(int[] arr,int left, int right) {
@@ -38,7 +39,7 @@ public class QuickSort {
 		int temp = 0; //临时变量，作为交换时使用
 		//while循环的目的是让比pivot 值小放到左边
 		//比pivot 值大放到右边
-		while( l < r) { 
+		while( l < r) {
 			//在pivot的左边一直找,找到大于等于pivot值,才退出
 			while( arr[l] < pivot) {
 				l += 1;
@@ -52,12 +53,12 @@ public class QuickSort {
 			if( l >= r) {
 				break;
 			}
-			
+
 			//交换
 			temp = arr[l];
 			arr[l] = arr[r];
 			arr[r] = temp;
-			
+			//int[] arr = {-9,78,0,23,-567,70, -1,900, 4561};
 			//如果交换完后，发现这个arr[l] == pivot值 相等 r--， 前移
 			if(arr[l] == pivot) {
 				r -= 1;
@@ -67,7 +68,7 @@ public class QuickSort {
 				l += 1;
 			}
 		}
-		
+
 		// 如果 l == r, 必须l++, r--, 否则为出现栈溢出
 		if (l == r) {
 			l += 1;
---
+++ DataStructures/src/src/per/ds/sort/QuickSort1.java
@@ -0,0 +1,203 @@
+package src.per.ds.sort;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class QuickSort1 {
+
+    public static void main(String[] args) {
+        int[] arr = {-9, 78, 0, 23, -567, 70, -1, 900, 4561, 0};
+
+        //测试快排的执行速度
+        // 创建要给80000个的随机的数组
+//		int[] arr = new int[8000000];
+//		for (int i = 0; i < 8000000; i++) {
+//			arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
+//		}
+//
+//		System.out.println("排序前");
+//		Date data1 = new Date();
+//		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//		String date1Str = simpleDateFormat.format(data1);
+//		System.out.println("排序前的时间是=" + date1Str);
+
+        quickSort_5(arr, 0, arr.length - 1);
+
+//		Date data2 = new Date();
+//		String date2Str = simpleDateFormat.format(data2);
+//		System.out.println("排序前的时间是=" + date2Str);
+        System.out.println("arr=" + Arrays.toString(arr));
+    }
+
+    public static void quickSort(int[] arr, int left, int right) {
+        int l = left;
+        int r = right;
+        int pivot = arr[(left + right) / 2];
+        int temp = 0;
+        while (l < r) {
+            while (arr[l] < pivot) {
+                l++;
+            }
+            while (arr[r] > pivot) {
+                r--;
+            }
+            if (l >= r) {
+                break;
+            }
+            temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            if (arr[l] == pivot) {
+                r--;
+            }
+            if (arr[r] == pivot) {
+                l++;
+            }
+        }
+        if (l == r) {
+            l++;
+            r--;
+        }
+        if (left < r) {
+            quickSort(arr, left, r);
+        }
+        if (right > l) {
+            quickSort(arr, l, right);
+        }
+
+    }
+
+    public static void quickSort_1(int[] arr, int left, int right) {
+        int l = left;
+        int r = right;
+        int pivot = arr[(left + right) / 2];
+        int temp = 0;
+        while (l < r) {
+            while (arr[l] < pivot) l++;
+            while (arr[r] > pivot) r--;
+            if (l >= r) {
+                break;
+            }
+            temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            if (arr[l] == pivot) {
+                r--;
+            }
+            if (arr[r] == pivot) {
+                l++;
+            }
+        }
+        if (l == r) {
+            l++;
+            r--;
+        }
+        if (left < r) {
+            quickSort_1(arr, left, r);
+        }
+        if (l < right) {
+            quickSort_1(arr, l, right);
+        }
+
+    }
+
+    public static void quickSort_2(int[] arr, int left, int right) {
+        int l = left;
+        int r = right;
+        int pivot = arr[(left + right) / 2];
+        int temp = 0;
+        while (l < r) {
+            while (arr[l] < pivot) l++;
+            while (arr[r] > pivot) r--;
+            if (l >= r) {
+                break;
+            }
+            temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            if (arr[l] == pivot) r--;
+            if (arr[r] == pivot) l--;
+        }
+        if (l == r) {
+            l++;
+            r--;
+        }
+        if (left < r) quickSort_2(arr, left, r);
+        if (right > l) quickSort_2(arr, l, right);
+    }
+
+    public static void quickSort_3(int[] arr, int left, int right) {
+        int l = left;
+        int r = right;
+        int pivot = arr[(right + left) / 2];
+        int temp = 0;
+        while (l < r) {
+            while (arr[l] < pivot) l++;
+            while (arr[r] > pivot) r--;
+            if (l >= r) {
+                break;
+            }
+            temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            if (arr[l] == pivot) r--;
+            if (arr[r] == pivot) l--;
+        }
+        if (l == r) {
+            l++;
+            r--;
+        }
+        if (left < r) quickSort_3(arr, left, r);
+        if (l < right) quickSort_3(arr, l, right);
+    }
+
+    public static void quickSort_4(int[] arr, int left, int right) {
+        int r=right;
+        int l=left;
+        int pivot=arr[(left+right)/2];
+        int temp=0;
+        while (l<r){
+            while (arr[l]<pivot) l++;
+            while (arr[r]>pivot) r--;
+            if(l>=r) break;
+            temp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=temp;
+            if(arr[l]==pivot) r--;
+            if(arr[r]==pivot) l++;
+        }
+        if(l==r){
+            l++;
+            r--;
+        }
+        if(left<r) quickSort_4(arr,left,r);
+        if(l<right) quickSort_4(arr,l,right);
+    }
+
+    public static void quickSort_5(int[] arr, int left, int right) {
+        int r=right;
+        int l=left;
+        int pivot =arr[(right+left)/2];
+        int temp=0;
+        while (l<r){
+            while (arr[l]<pivot) l++;
+            while (arr[r]>pivot) r--;
+            if(l>=r) break;
+            temp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=temp;
+            if(arr[l]==pivot) r--;
+            if (arr[r]==pivot) l++;
+        }
+        if(l==r){
+            l++;
+            r--;
+        }
+        if(left<r) quickSort_5(arr,left,r);
+        if(l<right) quickSort_5(arr,l,right);
+
+    }
+
+
+}
