5f5b9e405d366472de7fa5f5dd0a532e54ae3495
CDX <1138224907@qq.com>
2020-06-25 22:09:45 +0800

    算法：线性查找，二分查找

---
+++ DataStructures/src/src/per/ds/search/BinarySearch_1.java
@@ -0,0 +1,96 @@
+package src.per.ds.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//注意：使用二分查找的前提是 该数组是有序的.
+public class BinarySearch_1 {
+
+	public static void main(String[] args) {
+		//int arr[] = { 1, 8, 10, 89,1000,1000, 1234 };
+		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 12, 13,14,15,16,17,18,19,20 };
+		
+
+		//
+//		int resIndex = binarySearch(arr, 0, arr.length - 1, 1000);
+//		System.out.println("resIndex=" + resIndex);
+		
+		List<Integer> resIndexList = binarySearch2(arr, 0, arr.length - 1, 1);
+		System.out.println("resIndexList=" + resIndexList);
+	}
+
+	// 二分查找算法
+	/**
+	 * 
+	 * @param arr
+	 *            数组
+	 * @param left
+	 *            左边的索引
+	 * @param right
+	 *            右边的索引
+	 * @param findVal
+	 *            要查找的值
+	 * @return 如果找到就返回下标，如果没有找到，就返回 -1
+	 */
+	public static int binarySearch(int[] arr, int left, int right, int findVal) {
+		if(left>right){
+			return -1;
+		}
+		int mid =(left +right)/2;
+		int midVal =arr[mid];
+		if(findVal>midVal){
+			return binarySearch(arr,mid+1,right,findVal);
+		}else if(findVal<midVal){
+			return binarySearch(arr,left,mid-1,findVal);
+		}else {
+			return mid;
+		}
+	}
+	
+	//完成一个课后思考题:
+	/*
+	 * 课后思考题： {1,8, 10, 89, 1000, 1000，1234} 当一个有序数组中，
+	 * 有多个相同的数值时，如何将所有的数值都查找到，比如这里的 1000
+	 * 
+	 * 思路分析
+	 * 1. 在找到mid 索引值，不要马上返回
+	 * 2. 向mid 索引值的左边扫描，将所有满足 1000， 的元素的下标，加入到集合ArrayList
+	 * 3. 向mid 索引值的右边扫描，将所有满足 1000， 的元素的下标，加入到集合ArrayList
+	 * 4. 将Arraylist返回
+	 */
+
+	public static List<Integer> binarySearch2(int[] arr, int left, int right, int findVal) {
+		if(left>right){
+			return new ArrayList<Integer>();
+		}
+		int mid = (left+right)/2;
+		int midVal =arr[mid];
+		if(findVal>midVal){
+			return binarySearch2(arr,mid+1,right,findVal);
+		}else if(findVal<midVal){
+			return binarySearch2(arr,left,mid-1,findVal);
+		}else {
+			List<Integer> list =new ArrayList<>();
+			int temp =mid-1;
+			while (true){
+				if(temp<0 || arr[temp]!=findVal){
+					break;
+				}else {
+					list.add(temp);
+					temp--;
+				}
+			}
+			list.add(mid);
+			temp=mid+1;
+			while (true){
+				if(temp>arr.length || arr[temp]!=findVal){
+					break;
+				}else {
+					list.add(temp);
+					temp++;
+				}
+			}
+			return list;
+		}
+	}
+}
