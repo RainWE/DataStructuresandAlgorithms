24e572417be16688968474235956dd4483e4cf9b
CDX <1138224907@qq.com>
2020-08-18 15:54:31 +0800

    二叉搜索树

---
+++ LeetCode/src/per/leetcode/tree/KthLargest.java
@@ -0,0 +1,52 @@
+package per.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**二叉搜索树的第k大节点
+ 给定一棵二叉搜索树，请找出其中第k大的节点。
+ 示例 1:
+ 输入: root = [3,1,4,null,2], k = 1
+         3
+        / \
+       1   4
+        \
+ ?       2
+ 输出: 4
+ 示例 2:
+ 输入: root = [5,3,6,2,4,null,null,1], k = 3
+          5
+        / \
+       3   6
+      / \
+     2   4
+   /
+  1
+ 输出: 4
+ 限制：
+ 1 ≤ k ≤ 二叉搜索树元素个数
+ */
+public class KthLargest {
+    int k, res;
+    public int kthLargest(TreeNode root, int k) {
+       this.k=k;
+       dfs(root);
+       return res;
+    }
+    public void dfs(TreeNode root){
+        if (root==null) return;
+        dfs(root.right);
+        if (k==0) return;
+        k--;
+        if (k==0) res=root.val;
+        dfs(root.left);
+    }
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+}
